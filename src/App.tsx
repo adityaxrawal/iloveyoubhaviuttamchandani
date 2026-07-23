4912efd43591ac51e4fe049f42de5e92f8893ea4
Aditya Rawal <ar.adityarawal@gmail.com>
2026-07-23 20:56:10 +0530

    feat: implement texting page and chat parsing utilities with sample data integration

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,18 @@ import GlobalCursor from "./components/global-context/global-cursor";
 // import PixelRose from "./components/pixel-rose/pixel-rose";
 
 import LetterPage from "./page/letter";
+import TextingPage from "./page/texting";
+import PixelPage3D from "./page/3d-heart";
+import PixelRose from "./components/pixel-rose/pixel-rose";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path="/love" element={<PixelPage3D />} /> */}
-        {/* <Route path="/rose" element={<PixelRose />} /> */}
-        {/* <Route path="*" element={<PixelPage3D />} /> */}
+        <Route path="/love" element={<PixelPage3D />} />
+        <Route path="/rose" element={<PixelRose />} />
         <Route path="*" element={<LetterPage />} />
+        <Route path="texting" element={<TextingPage />} />
       </Routes>
       <GlobalCursor />
     </BrowserRouter>
